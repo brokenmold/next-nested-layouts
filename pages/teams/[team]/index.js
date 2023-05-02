fb63c3cd4ddc2e9aa459bb6d8a7aae7216c498f4
brokenmold <jason@broken-mold.com>
2023-05-02 11:40:51 -0500

    cherry pick

--- pages/teams/[team]/index.js
+++ pages/teams/[team]/index.js
@@ -1,14 +1,7 @@
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
-
-
 import { TeamsPageLayout } from "../index";
-import nestLayout from "../../../layouts/team-layout";
-
-const getLayout = (page) => <NestedLayout>{page}</NestedLayout>;
-export const TeamPageLayout = nestLayout(TeamsPageLayout, getLayout);
-
+import nestLayout from "../../../utils/nestLayout";
 
 const TeamPage = () => {
   const router = useRouter();
@@ -26,11 +19,22 @@ const NestedLayout = ({ children }) => {
 
   return (
     <div>
-      <h1>Anything, anything.</h1>
-      <p>
-        But this time it's <strong>{team}</strong><br/>
-        Because that's what you typed.
-      </p>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href={`/teams/${team}`}>About</Link>
+            </li>
+            <li>
+              <Link href={`/teams/${team}/players`}>Players</Link>
+            </li>
+            <li>
+              <Link href={`/teams/${team}/fixtures`}>Fixtures</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section>{children}</section>
     </div>
   );
 };
